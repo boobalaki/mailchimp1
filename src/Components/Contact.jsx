e846d153f6649a28b6cef15bf5f829a6f731ea79
cp <boobalakiruthra@gmail.com>
2024-09-04 11:03:49 +0530

    first commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -46,12 +46,12 @@ const Contact = () => {
    <section class="contact-section section-padding">
                 <div class="container">
                     <div class="row justify-content-center">
-
+                    <div className='col-lg-6 col-12 mb-lg-5 mb-3'>
                         {/* <div class="col-lg-6 col-12 mb-lg-5 mb-3">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d250642.30275911014!2d76.81312334907187!3d11.01715683077899!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba859af2f971cb5%3A0x2fc1c81e183ed282!2sCoimbatore%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1724757598869!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div> */}
                       
-                        
+                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d250644.76835078944!2d76.80241706882452!3d11.014261489796738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba859af2f971cb5%3A0x2fc1c81e183ed282!2sCoimbatore%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1725353811621!5m2!1sen!2sin" width="600" height="450"allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
 
                         <div class="col-lg-5 col-12 mb-3 mx-auto">
                             <div class="contact-info-wrap">
